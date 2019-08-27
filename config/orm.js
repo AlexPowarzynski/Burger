2a0cf70b695ddb7bb3c5f59f789626501d0536aa
Alex Powarzynski <alexpowarzynski@gmail.com>
2019-08-27 15:54:12 -0700

    heroku

--- config/orm.js
+++ config/orm.js
@@ -38,7 +38,7 @@ function objToSql(ob) {
 // Object for all our SQL statement functions.
 var orm = {
     all: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
+        let queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
