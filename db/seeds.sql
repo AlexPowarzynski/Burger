c61cf0fd4ed01707212e94c2faf4791843f753d1
Alex Powarzynski <alexpowarzynski@gmail.com>
2019-08-26 20:01:05 -0700

    Functionality complete/Needs a new look

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, eaten) VALUES ('Monster Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Double', true);
+INSERT INTO burgers (name, eaten) VALUES ('Fried Egg Burger', true);
+INSERT INTO burgers (name) VALUES ('Lettuce Wrap');
